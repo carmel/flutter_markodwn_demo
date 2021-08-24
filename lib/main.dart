315a7b2fc5e56d12af32930f25fb253eb4ff2fdb
carmel <carmeltop@hotmail.com>
2021-08-24 19:11:08 +0800

    flutter issues#88642

---
+++ lib/main.dart
@@ -0,0 +1,85 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/scheduler.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_markdown/flutter_markdown.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key? key, required this.title}) : super(key: key);
+  final String title;
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  double position = 61.0;
+  double fontSize = 18;
+  Future<String> get data => rootBundle.loadString('assets/markdown_test_page.md');
+  late AutoScrollController controller;
+
+  @override
+  void initState() {
+    super.initState();
+    controller = AutoScrollController(
+        viewportBoundaryGetter: () => Rect.fromLTRB(0, 0, 0, MediaQuery.of(context).padding.bottom),
+        axis: Axis.vertical);
+
+    SchedulerBinding.instance!.addPostFrameCallback((_) {
+      setState(() {
+        controller.jumpTo(controller.position.maxScrollExtent * position / 100);
+      });
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: CustomScrollView(
+        shrinkWrap: true,
+        controller: controller,
+        slivers: <Widget>[
+          SliverAppBar(
+            floating: true,
+            elevation: 4,
+            automaticallyImplyLeading: false,
+            title: Text('flutter markdown demo'),
+          ),
+          FutureBuilder<String>(
+              future: data,
+              builder: (BuildContext context, AsyncSnapshot<String> snapshot) {
+                if (snapshot.connectionState == ConnectionState.done) {
+                  return PositionableMarkdown(
+                    controller: controller,
+                    data: snapshot.data ?? '',
+                    selectable: true,
+                    // bulletBuilder: ,
+                    styleSheet: MarkdownStyleSheet.fromTheme(Theme.of(context)).copyWith(
+                      blockSpacing: 14,
+                      p: TextStyle(fontSize: fontSize),
+                      listBullet: TextStyle(fontSize: fontSize),
+                      listIndent: 34,
+                    ),
+                  );
+                } else {
+                  return SliverToBoxAdapter(child: const CircularProgressIndicator());
+                }
+              })
+        ],
+      ),
+    );
+  }
+}
