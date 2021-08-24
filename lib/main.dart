adf72b963d8f6fd28eb2d4456dea550ace32bb1f
carmel <carmeltop@hotmail.com>
2021-08-24 21:44:46 +0800

    add initialScrollOffset param for controller & add Alignment param for PositionableMarkdown

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/scheduler.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_markdown/flutter_markdown.dart';
 
@@ -28,6 +27,7 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   double position = 61.0;
   double fontSize = 18;
+  final _wrapAlignment = WrapAlignment.start;
   Future<String> get data => rootBundle.loadString('assets/markdown_test_page.md');
   late AutoScrollController controller;
 
@@ -35,14 +35,15 @@ class _MyHomePageState extends State<MyHomePage> {
   void initState() {
     super.initState();
     controller = AutoScrollController(
+        initialScrollOffset: controller.position.maxScrollExtent * position / 100,
         viewportBoundaryGetter: () => Rect.fromLTRB(0, 0, 0, MediaQuery.of(context).padding.bottom),
         axis: Axis.vertical);
 
-    SchedulerBinding.instance!.addPostFrameCallback((_) {
-      setState(() {
-        controller.jumpTo(controller.position.maxScrollExtent * position / 100);
-      });
-    });
+    // SchedulerBinding.instance!.addPostFrameCallback((_) {
+    //   setState(() {
+    //     controller.jumpTo(controller.position.maxScrollExtent * position / 100);
+    //   });
+    // });
   }
 
   @override
@@ -63,17 +64,26 @@ class _MyHomePageState extends State<MyHomePage> {
               builder: (BuildContext context, AsyncSnapshot<String> snapshot) {
                 if (snapshot.connectionState == ConnectionState.done) {
                   return PositionableMarkdown(
-                    controller: controller,
-                    data: snapshot.data ?? '',
-                    selectable: true,
-                    // bulletBuilder: ,
-                    styleSheet: MarkdownStyleSheet.fromTheme(Theme.of(context)).copyWith(
-                      blockSpacing: 14,
-                      p: TextStyle(fontSize: fontSize),
-                      listBullet: TextStyle(fontSize: fontSize),
-                      listIndent: 34,
-                    ),
-                  );
+                      controller: controller,
+                      data: snapshot.data ?? '',
+                      selectable: true,
+                      styleSheet: MarkdownStyleSheet.fromTheme(Theme.of(context)).copyWith(
+                        blockSpacing: 14,
+                        p: TextStyle(fontSize: fontSize),
+                        listBullet: TextStyle(fontSize: fontSize),
+                        listIndent: 34,
+                        textAlign: _wrapAlignment,
+                        h1Align: _wrapAlignment,
+                        h2Align: _wrapAlignment,
+                        h3Align: _wrapAlignment,
+                        h4Align: _wrapAlignment,
+                        h5Align: _wrapAlignment,
+                        h6Align: _wrapAlignment,
+                        unorderedListAlign: _wrapAlignment,
+                        orderedListAlign: _wrapAlignment,
+                        blockquoteAlign: _wrapAlignment,
+                        codeblockAlign: _wrapAlignment,
+                      ));
                 } else {
                   return SliverToBoxAdapter(child: const CircularProgressIndicator());
                 }
